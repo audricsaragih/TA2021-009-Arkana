1598343482fe77009b266c9cfdbd372e6abdc0b8
lisasim <49404284+lisasim@users.noreply.github.com>
2021-05-21 06:50:27 +0000

    Add files via upload

--- morpheus_chair/morpheus_chair_pkg/scripts/motor_driver.py
+++ morpheus_chair/morpheus_chair_pkg/scripts/motor_driver.py
@@ -1,17 +1,31 @@
 #!/usr/bin/env python
 import rospy
-import RPi.GPIO as GPIO
+##import RPi.GPIO as GPIO
 import time
+
+## edit : import pwm message
+from test_tutorials.msg import Pwm
+from std_msgs.msg import String
+##
 
 class MotorDriver(object):
 
-    def __init__(self, wheel_distance=0.098, wheel_diameter=0.066, i_BASE_PWM=50, i_MULTIPLIER_STANDARD=0.1, i_MULTIPLIER_PIVOT=1.0, simple_mode = True):
+    def __init__(self, wheel_distance=0.098, wheel_diameter=0.066, i_BASE_PWM=50, i_MULTIPLIER_STANDARD=0.067, i_MULTIPLIER_PIVOT=0.65, simple_mode = True):
         """
         M1 = Right Wheel
         M2 = Left Wheel
         :param wheel_distance: Distance Between wheels in meters
         :param wheel_diameter: Diameter of the wheels in meters
         """
+        ## edit : set message and publisher
+        self.msg = Pwm()
+        self.pub = rospy.Publisher('test_pwm', Pwm, queue_size=10)
+	self.testpub = rospy.Publisher('chatter', String, queue_size=10)
+	self.testmsg = 'Semangat'
+
+        self.directions = ''
+        ##
+
         self.PIN = 18
         self.PWMA1 = 6
         self.PWMA2 = 13
@@ -23,7 +37,7 @@ class MotorDriver(object):
         self.PWM1 = 0
         self.PWM2 = 0
         self.BASE_PWM = i_BASE_PWM
-        self.MAX_PWM = 100
+        self.MAX_PWM = 255 ##edit skala max dari 100
 
         self.simple_mode = simple_mode
 
@@ -33,73 +47,90 @@ class MotorDriver(object):
         self.MULTIPLIER_STANDARD = i_MULTIPLIER_STANDARD
         self.MULTIPLIER_PIVOT = i_MULTIPLIER_PIVOT
 
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setwarnings(False)
-        GPIO.setup(self.PIN, GPIO.IN, GPIO.PUD_UP)
-        GPIO.setup(self.PWMA1, GPIO.OUT)
-        GPIO.setup(self.PWMA2, GPIO.OUT)
-        GPIO.setup(self.PWMB1, GPIO.OUT)
-        GPIO.setup(self.PWMB2, GPIO.OUT)
-        GPIO.setup(self.D1, GPIO.OUT)
-        GPIO.setup(self.D2, GPIO.OUT)
-        self.p1 = GPIO.PWM(self.D1, 500)
-        self.p2 = GPIO.PWM(self.D2, 500)
-        self.p1.start(self.PWM1)
-        self.p2.start(self.PWM2)
 
-    def __del__(self):
-        GPIO.cleanup()
+        ##GPIO.setmode(GPIO.BCM)
+        ##GPIO.setwarnings(False)
+        ##GPIO.setup(self.PIN, GPIO.IN, GPIO.PUD_UP)
+        ##GPIO.setup(self.PWMA1, GPIO.OUT)
+        ##GPIO.setup(self.PWMA2, GPIO.OUT)
+        ##GPIO.setup(self.PWMB1, GPIO.OUT)
+        ##GPIO.setup(self.PWMB2, GPIO.OUT)
+        ##GPIO.setup(self.D1, GPIO.OUT)
+        ##GPIO.setup(self.D2, GPIO.OUT)
+        ##self.p1 = GPIO.PWM(self.D1, 500)
+        ##self.p2 = GPIO.PWM(self.D2, 500)
+        ##self.p1.start(self.PWM1)
+        ##self.p2.start(self.PWM2)
 
-    def set_motor(self, A1, A2, B1, B2):
-        GPIO.output(self.PWMA1, A1)
-        GPIO.output(self.PWMA2, A2)
-        GPIO.output(self.PWMB1, B1)
-        GPIO.output(self.PWMB2, B2)
+    ##def __del__(self):
+        ##GPIO.cleanup()
 
-    def forward(self):
-        self.set_motor(0, 1, 0, 1)
+    ##def set_motor(self, A1, A2, B1, B2):
+        ##GPIO.output(self.PWMA1, A1)
+        ##GPIO.output(self.PWMA2, A2)
+        ##GPIO.output(self.PWMB1, B1)
+        ##GPIO.output(self.PWMB2, B2)
 
-    def stop(self):
-        self.set_motor(0, 0, 0, 0)
+    ##def forward(self):
+        ##self.set_motor(0, 1, 0, 1)
 
-
-    def reverse(self):
-        self.set_motor(1, 0, 1, 0)
+    ##def stop(self):
+        ##self.set_motor(0, 0, 0, 0)
 
 
-    def left(self):
-        self.set_motor(0, 1, 0, 0)
-
-    def left_reverse(self):
-        self.set_motor(1, 0, 0, 0)
-
-    def pivot_left(self):
-        self.set_motor(1, 0, 0, 1)
+    ##def reverse(self):
+        ##self.set_motor(1, 0, 1, 0)
 
 
-    def right(self):
-        self.set_motor(0, 0, 0, 1)
+    ##def left(self):
+        ##self.set_motor(0, 1, 0, 0)
 
-    def right_reverse(self):
-        self.set_motor(0, 0, 1, 0)
+    ##def left_reverse(self):
+        ##self.set_motor(1, 0, 0, 0)
 
-    def pivot_right(self):
-        self.set_motor(0, 1, 1, 0)
+    ##def pivot_left(self):
+        ##self.set_motor(1, 0, 0, 1)
 
-    def set_M1M2_speed(self, rpm_speedM1, rpm_speedM2, multiplier):
+    ##def right(self):
+        ##self.set_motor(0, 0, 0, 1)
 
-        self.set_M1_speed(rpm_speedM1, multiplier)
-        self.set_M2_speed(rpm_speedM2, multiplier)
+    ##def right_reverse(self):
+        ##self.set_motor(0, 0, 1, 0)
 
-    def set_M1_speed(self, rpm_speed, multiplier):
+    ##def pivot_right(self):
+        ##self.set_motor(0, 1, 1, 0)
 
-        self.PWM1 = min(int((rpm_speed * multiplier) * self.BASE_PWM), self.MAX_PWM)
+    def set_M1M2_speed(self, rpm_speedM1, rpm_speedM2, multiplier, directions): ##edit : tambah parameter directions
+
+        ##self.set_M1_speed(rpm_speedM1, multiplier, directions)
+        ##self.set_M2_speed(rpm_speedM2, multiplier, directions)
+
+        ## edit
+        self.PWM1 = min(int(((rpm_speedM1 * multiplier) * self.BASE_PWM)*255/100), self.MAX_PWM) ##edit skala ke max 255
+        self.PWM2 = min(int((rpm_speedM2 * multiplier * self.BASE_PWM)*255/100), self.MAX_PWM) ##edit skala ke max 255
+        self.msg.directions = directions
+        self.msg.pwm1 = self.PWM1
+        self.msg.pwm2 = self.PWM2
+        print("M1="+str(self.PWM1))
+        print("M2="+str(self.PWM2))
+        print("Direction="+directions)
+        rospy.loginfo(self.msg)
+	rospy.loginfo(self.testmsg)
+	self.testpub.publish(self.testmsg)
+        self.pub.publish(self.msg)
+        ##
+
+    def set_M1_speed(self, rpm_speed, multiplier, directions):
+
+        ##self.PWM1 = min(int((rpm_speed * multiplier) * self.BASE_PWM), self.MAX_PWM)
+        self.PWM1 = min(int(((rpm_speed * multiplier) * self.BASE_PWM)*255/100), self.MAX_PWM) ##edit skala ke max 255
         self.p1.ChangeDutyCycle(self.PWM1)
         print("M1="+str(self.PWM1))
 
-    def set_M2_speed(self, rpm_speed, multiplier):
+    def set_M2_speed(self, rpm_speed, multiplier, directions):
 
-        self.PWM2 = min(int(rpm_speed * multiplier * self.BASE_PWM), self.MAX_PWM)
+        ##self.PWM2 = min(int(rpm_speed * multiplier * self.BASE_PWM), self.MAX_PWM)
+        self.PWM2 = min(int((rpm_speed * multiplier * self.BASE_PWM)*255/100), self.MAX_PWM) ##edit skala ke max 255
         self.p2.ChangeDutyCycle(self.PWM2)
         print("M2="+str(self.PWM2))
 
@@ -159,68 +190,90 @@ class MotorDriver(object):
 
         if right_wheel_rpm > 0.0 and left_wheel_rpm > 0.0:
             #print("All forwards")
-            self.set_M1M2_speed(abs(right_wheel_rpm), abs(left_wheel_rpm), self.MULTIPLIER_STANDARD)
+            ##self.set_M1M2_speed(abs(right_wheel_rpm), abs(left_wheel_rpm), self.MULTIPLIER_STANDARD)
 
             if self.simple_mode:
                 # We make it turn only on one wheel
                 if right_wheel_rpm > left_wheel_rpm:
                     #print("GO FORWARDS RIGHT")
-                    self.right()
+                    ##self.right()
+                    self.directions = 'right'
                 if right_wheel_rpm < left_wheel_rpm:
                     #print("GO FORWARDS LEFT")
-                    self.left()
+                    ##self.left()
+                    self.directions = 'left'
                 if right_wheel_rpm == left_wheel_rpm:
                     #print("GO FORWARDS")
-                    self.forward()
+                    ##self.forward()
+                    self.directions = 'forward'
             else:
                 #print("GO FORWARDS")
-                self.forward()
+                ##self.forward()
+                self.directions = 'forward'
 
+            self.set_M1M2_speed(abs(right_wheel_rpm), abs(left_wheel_rpm), self.MULTIPLIER_STANDARD, self.directions) ##edit
 
 
         elif right_wheel_rpm > 0.0 and left_wheel_rpm == 0.0:
             #print("Right Wheel forwards, left stop")
-            self.set_M1M2_speed(abs(right_wheel_rpm), abs(left_wheel_rpm), self.MULTIPLIER_STANDARD)
-            self.left()
+            ##self.set_M1M2_speed(abs(right_wheel_rpm), abs(left_wheel_rpm), self.MULTIPLIER_STANDARD)
+            ##self.left()
+            self.directions = 'left'
+            self.set_M1M2_speed(abs(right_wheel_rpm), abs(left_wheel_rpm), self.MULTIPLIER_STANDARD, self.directions) ##edit
 
         elif right_wheel_rpm > 0.0 and left_wheel_rpm < 0.0:
             #print("Right Wheel forwards, left backwards --> Pivot left")
-            self.set_M1M2_speed(abs(right_wheel_rpm), abs(left_wheel_rpm), self.MULTIPLIER_PIVOT)
-            self.pivot_left()
+            ##self.set_M1M2_speed(abs(right_wheel_rpm), abs(left_wheel_rpm), self.MULTIPLIER_PIVOT)
+            ##self.pivot_left()
+            self.directions = 'pivot_left'
+            self.set_M1M2_speed(abs(right_wheel_rpm), abs(left_wheel_rpm), self.MULTIPLIER_PIVOT, self.directions) ##edit
+
         elif right_wheel_rpm == 0.0 and left_wheel_rpm > 0.0:
             #print("Right stop, left forwards")
-            self.set_M1M2_speed(abs(right_wheel_rpm), abs(left_wheel_rpm), self.MULTIPLIER_STANDARD)
-            self.right()
+            ##self.set_M1M2_speed(abs(right_wheel_rpm), abs(left_wheel_rpm), self.MULTIPLIER_STANDARD)
+            ##self.right()
+            self.directions = 'right'
+            self.set_M1M2_speed(abs(right_wheel_rpm), abs(left_wheel_rpm), self.MULTIPLIER_STANDARD, self.directions) ##edit
 
         elif right_wheel_rpm < 0.0 and left_wheel_rpm > 0.0:
             #print("Right backwards, left forwards --> Pivot right")
-            self.set_M1M2_speed(abs(right_wheel_rpm), abs(left_wheel_rpm), self.MULTIPLIER_PIVOT)
-            self.pivot_right()
+            ##self.set_M1M2_speed(abs(right_wheel_rpm), abs(left_wheel_rpm), self.MULTIPLIER_PIVOT)
+            ##self.pivot_right()
+            self.directions = 'pivot_right'
+            self.set_M1M2_speed(abs(right_wheel_rpm), abs(left_wheel_rpm), self.MULTIPLIER_PIVOT, self.directions) ##edit
+
         elif right_wheel_rpm < 0.0 and left_wheel_rpm < 0.0:
             #print("All backwards")
-            self.set_M1M2_speed(abs(right_wheel_rpm), abs(left_wheel_rpm), self.MULTIPLIER_STANDARD)
+            ##self.set_M1M2_speed(abs(right_wheel_rpm), abs(left_wheel_rpm), self.MULTIPLIER_STANDARD)
 
             if self.simple_mode:
                 # We make it turn only on one wheel
                 if abs(right_wheel_rpm) > abs(left_wheel_rpm):
                     #print("GO BACKWARDS RIGHT")
-                    self.right_reverse()
+                    ##self.right_reverse()
+                    self.directions = 'right_reverse'
                 if abs(right_wheel_rpm) < abs(left_wheel_rpm):
                     #print("GO BACKWARDS LEFT")
-                    self.left_reverse()
+                    #self.left_reverse()
+                    self.directions = 'left_reverse'
                 if right_wheel_rpm == left_wheel_rpm:
                     #print("GO BACKWARDS")
-                    self.reverse()
+                    ##self.reverse()
+                    self.directions = 'reverse'
             else:
-                self.reverse()
+                ##self.reverse()
+                self.directions = 'reverse'
 
-
+            self.set_M1M2_speed(abs(right_wheel_rpm), abs(left_wheel_rpm), self.MULTIPLIER_STANDARD, self.directions) ##edit
 
 
         elif right_wheel_rpm == 0.0 and left_wheel_rpm == 0.0:
             #print("Right stop, left stop")
-            self.set_M1M2_speed(abs(right_wheel_rpm), abs(left_wheel_rpm), self.MULTIPLIER_STANDARD)
-            self.stop()
+            ##self.set_M1M2_speed(abs(right_wheel_rpm), abs(left_wheel_rpm), self.MULTIPLIER_STANDARD)
+            ##self.stop()
+            self.directions = 'stop'
+            self.set_M1M2_speed(abs(right_wheel_rpm), abs(left_wheel_rpm), self.MULTIPLIER_STANDARD, self.directions) ##edit
+
         else:
             assert False, "A case wasn't considered==>"+str(right_wheel_rpm)+","+str(left_wheel_rpm)
             pass
@@ -244,5 +297,4 @@ class MotorDriver(object):
 
 
         self.set_wheel_movement(right_wheel_rpm, left_wheel_rpm)
-
 
